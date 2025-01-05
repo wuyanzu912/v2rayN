157b742371ea82ed1d393dab2317ad485980fff4
wuyanzu912 <jlynette145@gmail.com>
2025-01-06 00:58:04 +0800

    v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs   29

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -26,6 +26,8 @@ namespace v2rayN.Desktop.Views
         public MainWindow()
         {
             InitializeComponent();
+            // 禁用检查更新按钮
+            menuCheckUpdate.IsEnabled = false;
 
             _config = AppHandler.Instance.Config;
             _manager = new WindowNotificationManager(TopLevel.GetTopLevel(this)) { MaxItems = 3, Position = NotificationPosition.BottomRight };
